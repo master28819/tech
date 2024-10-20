8a261a170e3b21b814ebdeff4737063048070561
inj-krish19 <kglivee19@gmail.com>
2024-10-20 09:32:43 +0530

    Checking Environment Variables Compatibility

--- src/main/java/com/example/tech/EnvController.java
+++ src/main/java/com/example/tech/EnvController.java
@@ -3,15 +3,23 @@ package com.example.tech;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @RestController
 public class EnvController {
 
+
+    private Dotenv dotenv = Dotenv.load();
     @Value("${WHATS_NAME:not working}")
     private String myEnvVariable;
 
     @GetMapping("/env")
     public String getEnvVariable() {
+    	
+    	if( myEnvVariable.equals("not working") ) {
+    		myEnvVariable = dotenv.get("WHATS_NAME");
+    	}
+    	
         return "Value of WHATS_NAME: " + myEnvVariable;
     }
 }
